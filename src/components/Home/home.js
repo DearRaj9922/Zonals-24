ea611c90591faba8dd664f027efd41296ffd7c1b
Divya Raj <draj56227@gmail.com>
2024-07-03 13:48:51 +0530

    buttons added to map

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import mapimg from '../../assets/India Map 1.webp';
 import './home.css';
+import zonaltext from "../../assets/Zonalstext.webp";
+import thomsologo from "../../assets/logo.png";
 
 export default function Home() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add('blur');
+    const timer = setTimeout(() => {
+      setAnimate(true);
+      document.body.classList.remove('blur');
+    }, 2000); // 1 second delay
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const handleMouseEnter = (areaClass) => {
     document.body.classList.add(areaClass);
   };
@@ -35,6 +49,20 @@ export default function Home() {
           onMouseEnter={() => handleMouseEnter('zoom-area4')}
           onMouseLeave={() => handleMouseLeave('zoom-area4')}
         ></div>
+      </div>
+      <div className="button-section">
+        <div className="logo-thomso">
+          <img src={thomsologo} className='thomso-img' alt='tho-logo'/>
+        </div>
+      <div className={`zonalstext ${animate ? 'scale-down' : ''}`}>
+        <img src={zonaltext} className="zonals-text" alt="zonalstext" style={{ animation: 'popUp 2s forwards' }} />
+      </div>
+      <div className={`buttons ${animate ? 'scale-down' : ''}`}>
+        <button className='jaipur'>Jaipur</button>
+        <button className='chandigarh'>Chandigarh</button>
+        <button className='lucknow'>Lucknow</button>
+        <button className='banglore'>Banglore</button>
+      </div>
       </div>
     </div>
   );
